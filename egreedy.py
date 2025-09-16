a4c7ba647fe7a003699f2cca0be492d291d33bef
Basito0 <76927670+Basito0@users.noreply.github.com>
2025-09-16 15:45:38 -0300

    egreedy added

---
+++ egreedy.py
@@ -0,0 +1,160 @@
+import os
+import sys
+import random
+
+def ChooseInstance(instance):
+    dataset_path = "dataset_grafos_no_dirigidos"
+    first_dataset_path = os.path.join(dataset_path, "new_1000_dataset")
+    second_dataset_path = os.path.join(dataset_path, "new_2000_dataset")
+    third_dataset_path = os.path.join(dataset_path, "new_3000_dataset")
+
+    script_dir = os.path.dirname(__file__)
+
+    first_dataset_path = os.path.join(script_dir, first_dataset_path)
+    second_dataset_path = os.path.join(script_dir, second_dataset_path)
+    third_dataset_path = os.path.join(script_dir, third_dataset_path)
+
+    if instance == "1000":
+        return first_dataset_path
+    elif instance == "2000":
+        return second_dataset_path
+    elif instance == "3000":
+        return third_dataset_path
+        
+
+if __name__ ==  "__main__":
+
+    #Esta primera parte es para tomar los argumentos y usarlos para tener los archivos de cierto directorio
+    choosen_dataset_path = ""
+    determinismo = 10
+    size_lista = 3
+
+
+    if len(sys.argv) == 5:
+        choosen_dataset_path = ChooseInstance(sys.argv[2])
+        determinismo = float(sys.argv[3])
+        size_lista = int(sys.argv[4])
+    else:
+        print("el modo correcto de ejecución es: python3 egreedy.py -i <instancia-problema> <nivel-determinismo> <tamaño-lista-mejores>")
+        sys.exit()
+
+    directory = os.fsencode(choosen_dataset_path)
+    
+
+    #ya obtenido el directorio aquí hay que iterar por cada file instancia
+    for f in os.listdir(directory):
+        filename = os.fsdecode(f)
+        
+        nodes_selected = []
+
+        with open(os.path.join(choosen_dataset_path, filename) , "r") as file:
+            #print("processing " + filename)
+
+            total_nodes = int(file.readline()) #Parece que esta línea es el número de nodos nomás
+            nodes_in_mis = 0
+
+            graph_matrix = [[0 for _ in range(total_nodes)] for _ in range(total_nodes)]
+
+            #este loop rellena la matriz de adyacencia
+            #print("filling adjacency matrix...")
+            for line in file:
+                ab_nodes = line.split()
+                a_node = int(ab_nodes[0])
+                b_node = int(ab_nodes[1])
+
+                graph_matrix[a_node][b_node] += 1
+                
+            total_neighbors = [0 for _ in range(total_nodes)]
+            
+            #print("counting neighbours...")
+            #aquí se cuentan los vecinos de cada nodo
+            for i in range(total_nodes):
+                for j in range(total_nodes):
+                    if(graph_matrix[i][j] == 1):
+                        total_neighbors[i] += 1
+
+            remaining_nodes = total_nodes
+
+            while remaining_nodes > 0:
+                #este loop es para ir borrando el nodo con menos vecinos
+                #si el nodo es -1 entonces ya se agregó al MIS
+                min_value = total_nodes
+                min_index=0
+
+                #aquí se ve qué nodo tiene menos vecinos y su índice
+                #print("selecting node with the least neighbours...")
+                for j in range(len(total_neighbors)):
+                    if total_neighbors[j] < min_value and total_neighbors[j] > -1:
+                        min_value = total_neighbors[j]
+                        min_index = j
+
+                #Este paso es exlusivo del e-greedy: tiramos una moneda (de 0 a 100). Si la moneda es menor a determinismo
+                #entonces hacemos lista de mejores candidatos y seleccionamos uno al azar.
+                randvalue = random.randint(0, 100)
+                if randvalue < determinismo:
+                    #print("random!")
+                    best_nodes = []
+                    best_nodes = total_neighbors[:]
+                    indexed_list = list(enumerate(best_nodes))
+                    sorted_indexed_list = sorted(indexed_list, key=lambda item: item[1])
+                    
+                    new_tuple = tuple(x for x in sorted_indexed_list if x[1] != -1)
+                    
+                    index_select = random.randint(0, size_lista-1)
+                    min_value = new_tuple[index_select][1]
+                    min_index = new_tuple[index_select][0]
+
+                #print("vecinos del seleccionado: " + str(min_value) + " con índice " + str(min_index))
+                #print("node selected: " + str(min_index))
+                nodes_selected.append(min_index)
+                total_neighbors[min_index] = -1
+                nodes_in_mis += 1
+
+                #aquí borramos el nodo elegido y sus vecinos de la matriz de adyacencia
+                #print("cleaning selected node...")
+                for i in range(len(total_neighbors)):
+
+                    #este loop es para borrar todo rastro del vecino
+                    if graph_matrix[min_index][i] == 1:
+                        total_neighbors[i] = -1
+                        for j in range(len(total_neighbors)):
+                            graph_matrix[j][i] = 0
+                            graph_matrix[i][j] = 0
+
+                    #y aquí borramos el nodo
+                    graph_matrix[i][min_index] = 0
+                    graph_matrix[min_index][i] = 0
+
+                #se reinician los nodos de cada vecino menos de los elegidos (y debería borrar a sus vecinos igual)
+                #print("resetting neighbours...")
+                for i in range(len(total_neighbors)):
+                    if total_neighbors[i]>-1:
+                        total_neighbors[i] = 0
+                
+                remaining_nodes = 0
+                #aquí se ve cuantos nodos quedan y se agregan al mis los que no tienen vecinos
+                #print("counting remaining nodes and adding to MIS")
+                for i in range(total_nodes):
+                    if total_neighbors[i] == -1:
+                        continue
+                    
+                    for j in range(total_nodes):
+                        if(graph_matrix[i][j] == 1):
+                            total_neighbors[i] += 1
+
+                    if total_neighbors[i] > 0:
+                        remaining_nodes += 1
+                    elif total_neighbors[i] == 0:
+                        nodes_in_mis += 1
+                        total_neighbors[i] = -1
+                #print("nodes in mis: " + str(nodes_in_mis))
+                #print("remaining nodes: " + str(remaining_nodes))
+                #print(nodes_selected)
+                
+
+            #print("repetidos? " + str(len(nodes_selected) != len(set(nodes_selected))))
+            print(nodes_in_mis)
+            
+        
+
+    
